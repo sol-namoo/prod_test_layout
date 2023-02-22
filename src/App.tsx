5cf3e775e95e1d8637b05eaedbed5ffbf41c9ff3
이솔 <lees@focusmediakorea.com>
2023-02-22 11:00:45 +0900

    feat: put header on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
+import Header from './Header';
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: layout</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
+    <>
+        <Header />
+        <div className="mt-10 text-3xl mx-auto max-w-6xl">
+            <div>Name: layout</div>
+            <div>Framework: react</div>
+            <div>Language: TypeScript</div>
+            <div>CSS: Tailwind</div>
+        </div>
+    </>
 );
 ReactDOM.render(<App />, document.getElementById("app"));

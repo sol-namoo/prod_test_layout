26ab8700e2044b015a058e6df230ac321eae6f15
이솔 <lees@focusmediakorea.com>
2023-02-22 14:28:32 +0900

    comment: add explanation on index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import Header from './Header';
 
 const App = () => (
     <>
-        <Header />
         <div className="mt-10 text-3xl mx-auto max-w-6xl">
             <div>Name: layout</div>
             <div>Framework: react</div>
